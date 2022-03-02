80a5eb4d17d531dca21cfce0ab197ddee16637ff
LuaanBarbosa <luancesar@outlook.com>
2022-03-02 15:10:46 -0300

    Landing Page Finalizada
    Landing page finalizada.
    Fazer ajustes de responsividade e adicionar botão "back-to-top"

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,21 +1,32 @@
-// window.onload = function(){
-//     document.querySelector(".menuMobile").addEventListener("click", function(){
-//         if (document.querySelector(".menu nav ul").style.display == 'flex'){
-//             document.querySelector(".menu nav ul").style.display = 'none';
-//         } else {
-//             document.querySelector(".menu nav ul").style.display = 'flex';
-//         }
-//     });
-// }
+$("a.scroll").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        let hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 5000, function() {
+            window.location.hash = hash;
+        });
+    }
+});
+$(document).ready(function () {
+    $(".back-to-top").css("display", "none");
+$("body").scroll(function() {
+    if($(this).scrollTop() == 0){
+        $(".back-to-top").css("display", "none");
+    } else {
+        $(".back-to-top").css("display", "block");
+    }
+    });
+$(".back-to-top").click(function() {
+    $("html, body").animate({scrollTop: 0}, 800);
+    });
+});
 
-// $("a.scroll").on('click', function(event) {
-//     if (this.hash !== "") {
-//         event.preventDefault();
-//         let hash = this.hash;
-//         $('html, body').animate({
-//             scrollTop: $(hash).offset().top
-//         }, 5000, function() {
-//             window.location.hash = hash;
-//         });
-//     }
-// });
+$("body").scroll(function() {
+    if($(this).scrollTop() == 0){
+    $(".back-to-top").css("display", "none");
+    } else {
+    $(".back-to-top").css("display", "block");
+    }
+});
