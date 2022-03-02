342cf53fc5fe16538013ef0a0d00271bd8018fad
LuaanBarbosa <luancesar@outlook.com>
2022-03-02 17:43:14 -0300

    Adicionado back-to-top

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,32 +1,36 @@
-$("a.scroll").on('click', function(event) {
+$("a.scroll").on('click', function (event) {
     if (this.hash !== "") {
         event.preventDefault();
         let hash = this.hash;
         $('html, body').animate({
             scrollTop: $(hash).offset().top
-        }, 5000, function() {
+        }, 5000, function () {
             window.location.hash = hash;
         });
     }
 });
-$(document).ready(function () {
-    $(".back-to-top").css("display", "none");
-$("body").scroll(function() {
-    if($(this).scrollTop() == 0){
-        $(".back-to-top").css("display", "none");
-    } else {
-        $(".back-to-top").css("display", "block");
-    }
-    });
-$(".back-to-top").click(function() {
-    $("html, body").animate({scrollTop: 0}, 800);
-    });
-});
 
-$("body").scroll(function() {
-    if($(this).scrollTop() == 0){
-    $(".back-to-top").css("display", "none");
-    } else {
-    $(".back-to-top").css("display", "block");
-    }
-});
+var btn = ("#back-to-top");
+
+btn.click(function() {
+    $('html, body').animate({scrollTop:0}, 'slow');
+})
+
+// $(document).ready(function () {
+//     // $(".back-to-top").css("display", "none");
+//     console.log($(this).scrollTop());
+
+//     $("body").scroll(function() {
+//         console.log($(this).scrollTop());
+
+//             if($(this).scrollTop() >= 0){
+//                 $(".back-to-top").css("display", "block");
+//             }
+//             if($(this).scrollTop() == 0){
+//                 $(".back-to-top").css("display", "none");
+//             }
+//     });
+//     $(".back-to-top").click(function() {
+//         $("html, body").animate({scrollTop: 0}, 200);
+//         });
+// });
